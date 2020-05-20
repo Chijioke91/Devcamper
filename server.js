51233e14f6294c2234d4a842001a4ad76880c078
= <=>
2020-05-20 23:16:51 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config/config.env',
+});
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.listen(
+  port,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port ${port}`
+  )
+);
